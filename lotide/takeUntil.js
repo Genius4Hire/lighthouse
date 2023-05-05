898bfcd45475eccffc3a789ff5ca4b2541108e80
Jordan Dennis <jordan.dennis@gmail.com>
2023-05-05 04:26:02 +0000

    First commit of takeUntil.js

---
+++ lotide/takeUntil.js
@@ -0,0 +1,50 @@
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log("😒");
+    return;
+  }
+
+  let isEqual = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      isEqual = false;
+    }
+  }
+
+  if (isEqual) {
+    console.log("🤌");
+  } else {
+    console.log("😒");
+  }
+};
+
+
+
+const takeUntil = function(array, callback) {
+  output = [];
+  for (item of array){
+    if (!callback(item)) {
+      output.push(item);
+    } else break;
+  }
+  return output;
+
+  // while (!callback(array)) {
+  //   output.push(callback[array]);
+  // } 
+  // return output;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+console.log(results2);

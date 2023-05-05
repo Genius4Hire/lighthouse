8fbb1923daa7e6efff973e1d33b5a4558a3729c4
Jordan Dennis <jordan.dennis@gmail.com>
2023-05-05 05:02:14 +0000

    First commit of findKey.js

---
+++ lotide/findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤌: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒 : ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const findKey = function(obj, callback) {
+  for (item in obj) {
+    if (callback(obj[item])) {
+      return item;
+    };
+  }
+  return undefined;
+}
+
+
+console.log( // => "noma"
+)
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")

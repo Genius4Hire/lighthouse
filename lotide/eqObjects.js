3c333c1e066bb6c7009fde134f7b0b1e6e4c5732
Jordan Dennis <jordan.dennis@gmail.com>
2023-05-06 01:09:42 +0000

    Used Object.keys() to simplfy ordered comparison

--- lotide/eqObjects.js
+++ lotide/eqObjects.js
@@ -6,26 +6,44 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  let result = true;
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+  return result;
+}
+
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
+
+const eqObjects = function(object1, object2) { 
+  //console.log(Object.keys(object1));
+  const obj1list = Object.keys(object1);
+  const obj2list = Object.keys(object2);
+  // console.log("k1",Object.keys(object1))
+  // console.log("k2",Object.keys(object2))
+  obj1list.sort();
+  obj2list.sort();
+  // console.log("--------------------")
+  // console.log("k1",Object.keys(object1))
+  // console.log("k2",Object.keys(object2))
+  return eqArrays(obj1list,obj2list);
 };
 
 
-const shirtObject = {
-  color : "red", 
-  size : "medium"
-}
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject , anotherShirtObject)); // => true
+console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
 
-const anotherShirtObject = {
-  color : "red",
-  size : "medium"
-}
-
-const longSleeveShirtObject
-
-eqObjects(shirtObject, anotherShirtObject);
-eqObjects(shirtObject, longSleeveShirtObject);
-
-
-}
+assertEqual(eqObjects(shirtObject , anotherShirtObject),true)
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject),false)
